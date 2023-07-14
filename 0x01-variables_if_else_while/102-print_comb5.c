b4db48cfd0804ceb549425709fcbeb8f6eed1d06
lordrhaegar <sparkzilla7@gmail.com>
2023-07-14 16:01:58 -0700

    New commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - the program entry point
+ * Description: This function prints all possible 
+ * combinations of two two-digit numbers.
+ * Return: the function retuens zero
+ */
+int main(void)
+{
+	int i, j;
+
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+
+	putchar('\n');
+
+
+	return (0);
+}
