5d92b69e4acf494b08d87f99382b2d1a73764aeb
lordrhaegar <sparkzilla7@gmail.com>
2023-07-14 06:49:43 -0700

    New Commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,6 +13,10 @@ int main(void)
 
         for (i = 97; i <= 122; i++)
         {
+		if (i == 101 || i == 113)
+		{
+			continue;
+		}
                 putchar(i);
         }
         putchar('\n');
