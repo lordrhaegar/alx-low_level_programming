fba9631b3e9e28dd89e0df3a03aaafcd60600443
lordrhaegar <sparkzilla7@gmail.com>
2023-07-13 17:22:50 -0700

    New Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - This is the main Function
  * Description: This function prints a string
  * Return: It returns zero
- * /
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
