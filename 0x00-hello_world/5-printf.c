59b0967260c90066302dc192e512fda78f990604
lordrhaegar <sparkzilla7@gmail.com>
2023-07-13 17:27:53 -0700

    New Commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main Function
+ * Description: This function prints a string
+ * Return: It returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
