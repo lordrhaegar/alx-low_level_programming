020b63914171958ff2aed8fc37374dbb1551851d
lordrhaegar <sparkzilla7@gmail.com>
2023-07-13 18:21:35 -0700

    New Commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (to comply with the requirements)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+	return (1);
+}
+
