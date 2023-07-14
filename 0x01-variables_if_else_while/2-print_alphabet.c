d8ec56378d7868fee7c0c6de6f4d5dc0ceaed89b
lordrhaegar <sparkzilla7@gmail.com>
2023-07-14 06:25:10 -0700

    New Commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the program entry point
+ * Description: This function prints alphabets in lowercase
+ * Return: the function retuens zero
+ */
+int main(void)
+{
+	char a[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+                putchar(a[i] + 32);
+        }
+        putchar('\n');
+
+	return (0);
+}
