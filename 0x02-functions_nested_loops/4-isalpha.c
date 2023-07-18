5d33597b377453f1d64d2d1bec8930fd052bc19b
lordrhaegar <sparkzilla7@gmail.com>
2023-07-18 08:23:01 -0700

    New commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry of the program
+ * Description - runs _islower() function
+ * Return: On success 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('-');
+	_putchar(r + '0');
+	r = _isalpha('a');
+	_putchar(r + '0');
+	r = _isalpha('B');
+	_putchar(r + '0');
+	r = _isalpha('b');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
+
+/**
+ * _isalpha - Alphabetical checker
+ * Description - checks if a character is alphabetical
+ * @c: The character to check
+ * Return: returns 0 if character is not alphabetical
+ * and returns 1 if character is alphabetical
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
