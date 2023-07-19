9451b58349519ca043d55a4233d03c55cfae40d5
lordrhaegar <sparkzilla7@gmail.com>
2023-07-19 13:25:33 -0700

    New commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print - print numbers
+ * Description - print numbers using _putchar function
+ * @n: number to print
+ * Return - void
+ */
+
+void print(int n)
+{
+	int digits[100];
+	int j;
+	int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	while (n > 0)
+	{
+		digits[i] = n % 10;
+		n /= 10;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		if (i == 1)
+		{
+			_putchar(' ');
+		}
+		_putchar(digits[j] + '0');
+	}
+}
+
+/**
+ * print_times_table - print times table
+ * Description - print times table from 0 - n
+ * @n: the number of times table to be printed
+ * Return - void
+ */
+void print_times_table(int n)
+{
+	int j;
+	int i = 0;
+
+	if (n >= 0 && n <= 15)
+	{
+		while (i <= n)
+		{
+			j = 0;
+			while (j <= n)
+			{
+				print(i * j);
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (i == 0 && j != n)
+				{
+					_putchar(' ');
+				}
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
