84ded870584074672ca1d7247ae83e4c570dacf8
lordrhaegar <sparkzilla7@gmail.com>
2023-07-20 03:07:21 -0700

    function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - Checks digit
+ * Description - checks if a number is a digit
+ * @c: the character to check
+ * Return: 1 if its a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multipply numbers
+ * Description - function to multiply 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return - returns the product
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
