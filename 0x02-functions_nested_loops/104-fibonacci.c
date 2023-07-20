a5454b3bd6624f4b230b3183211c9f03ae993ced
lordrhaegar <sparkzilla7@gmail.com>
2023-07-19 17:42:06 -0700

    new commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print - print numbers
+ * Description - print numbers using _putchar function
+ * @n: number to print
+ * Return - void
+ */
+int main(void)
+{
+	fabonacci(1,2);
+	return (0);
+}
+void printer(unsigned long n)
+{
+	int digits[100];
+	int j;
+	int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	while (n > 0)
+	{
+		digits[i] = n % 10;
+		n /= 10;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(digits[j] + '0');
+	}
+}
+
+/**
+ * times_table - print times table
+ * Description - print times table from 0 - 9
+ * Return - void
+ */
+void fabonacci(unsigned long a, unsigned long b)
+{
+	unsigned long i;
+	unsigned long result;
+	unsigned long temp;
+
+	printer(a);
+	_putchar(',');
+	_putchar(' ');
+	printer(b);
+	_putchar(',');
+	_putchar(' ');
+	for (i = 0; i < 98; i++)
+	{
+		result = a + b;
+		printer(result);
+		if (i != 97)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		temp = a;
+		a = b;
+		b = result;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void print(int n);
+void printer(unsigned long n);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+void fabonacci(unsigned long a, unsigned long b);
