46c76d9167ee90bfc838feff694f7f56287b6d50
lordrhaegar <sparkzilla7@gmail.com>
2023-07-21 13:45:58 -0700

    New commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * Description - prints triangle with specified length
+ * @size: the length of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		if (size == 0 || size < 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		for (j = i - 1; j < size - 1; j++)
+		{
+			_putchar(' ');
+		}
+		print_hash(i);
+		_putchar('\n');
+	}
+}
+
+/**
+ * print_hash - prints #
+ * Description - prints the number of hashes passed in
+ * @hash: number of hash to be printed
+ * Return: void
+ */
+void print_hash(int hash)
+{
+	int i;
+
+	for (i = 0; i < hash; i++)
+	{
+		_putchar('#');
+	}
+}
