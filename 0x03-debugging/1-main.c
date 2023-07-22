db5865aacc1830772a717ce20c8bb474a3df2eb1
lordrhaegar <sparkzilla7@gmail.com>
2023-07-22 02:26:03 -0700

    New commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Description: breaks infinity
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
