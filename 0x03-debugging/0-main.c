ec6808570c92ffe7437eb518ce57ec3a62f04b40
lordrhaegar <sparkzilla7@gmail.com>
2023-07-22 02:15:32 -0700

    New commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry point of the program
+ * Description - Check for positive or negative integers
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
