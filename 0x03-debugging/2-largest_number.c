8515cdca73ae6bb397e2a687158acdef768878aa
lordrhaegar <sparkzilla7@gmail.com>
2023-07-22 02:56:34 -0700

    New commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,11 +15,15 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > a && c > a)
+	{
+		largest = bc
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else
 	{
 		largest = c;
 	}
