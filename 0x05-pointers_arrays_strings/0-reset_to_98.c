5f4dc7a058466ed07eee12f9417df198f9c0b986
lordrhaegar <sparkzilla7@gmail.com>
2023-07-25 03:03:09 -0700

    function that updtaes the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - reset n 
+ * reset_to_98 - reset n
  * Description - changes the calue in the address passed to 98
- * @n - the pointer to the value
+ * @n: the pointer to the value
  * Return - void
  */
 void reset_to_98(int *n)
