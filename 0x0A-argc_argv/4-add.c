3d74f20465ace6e6202bc940a29b571f14caa75a
lordrhaegar <sparkzilla7@gmail.com>
2023-08-04 19:53:09 -0700

    program

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ * Description: prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, add;
+
+	add = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		add += atoi(argv[i]);
+	}
+	if (argc == 0)
+	{
+		printf("%c\n", '0');
+	}
+	else
+	{
+		printf("%d\n", add);
+	}
+
+	return (0);
+}
