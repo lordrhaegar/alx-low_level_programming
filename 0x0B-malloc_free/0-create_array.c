f3b9d999ef649b3a6cdef5ab7b82b67d720b4e6b
lordrhaegar <sparkzilla7@gmail.com>
2023-08-09 06:58:39 -0700

    function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *p;
-
+	
+	p = (char *) malloc(sizeof(char) * size);
 	if (p == NULL || size == 0)
 	{
 		return (NULL);
 	}
-	p = (char *) malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
