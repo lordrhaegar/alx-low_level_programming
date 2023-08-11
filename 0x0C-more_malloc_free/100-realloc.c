d990b74bf05819ca91ac8bf6af15d6ee1f62edf1
lordrhaegar <sparkzilla7@gmail.com>
2023-08-10 19:20:16 -0700

    function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	s = old_size < new_size ? old_size : new_size;
 	for (i = 0; i < s; i++)
-		(char *)p[i] = (char *)ptr[i];
+		*((char *)p + i) = *((char *)ptr + i);
 	free(ptr);
 	return (p);
 }
